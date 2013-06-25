4289a4f1866ddaa71a8a5e285eef3cc7a8a27fbf
Egor Lynko <flexoid@gmail.com>
2013-06-25 12:36:02 +0300

    Add location option to helper

--- lib/mongoid_sortable/helpers/sorting_helper.rb
+++ lib/mongoid_sortable/helpers/sorting_helper.rb
@@ -1,8 +1,7 @@
 module MongoidSortable
   module Helpers
     module SortingHelper
-      def sorting_link(sorting_criteria, field, name = nil, &block)
-        path_method = :"#{sorting_criteria.model_name.pluralize.underscore}_path"
+      def sorting_link(sorting_criteria, field, options = {}, &block)
         current_order_value = sorting_criteria.options.sort.try(:[], field.to_s)
         current_order = (current_order_value == 1) ? :asc : :desc
 
@@ -11,14 +10,24 @@ module MongoidSortable
 
         new_order = (current_order == :asc || !current_order_value) ? :desc : :asc
 
-        path = Rails.application.routes.url_helpers.
-          public_send(path_method, order_by: "#{field}_#{new_order}")
-        name ||= sorting_criteria.klass.human_attribute_name(field)
+        if options[:location]
+          uri = URI.parse(options[:location])
+          query = {order_by: "#{field}_#{new_order}"}.to_query
+          uri.query.present? ? uri.query += '&' : uri.query = ''
+          uri.query += query
+          location = uri.to_s
+        else
+          path_method = :"#{sorting_criteria.model_name.pluralize.underscore}_path"
+          location = Rails.application.routes.url_helpers.
+            public_send(path_method, order_by: "#{field}_#{new_order}")
+        end
+
+        name = options[:name] || sorting_criteria.klass.human_attribute_name(field)
 
         if block_given?
-          capture(name, path, current_order.to_s, !!current_order_value, &block)
+          capture(name, location, current_order.to_s, !!current_order_value, &block)
         else
-          link_to name, path, html_options
+          link_to name, location, html_options
         end
       end
     end
